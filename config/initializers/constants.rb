93b62a5f68a14e99f4cf32075e4e0ab2bd2bf246
Joni Toyryla <info@jonitoyryla.eu>
2024-11-18 14:42:05 +0200

    Some links changed to constants.

---
+++ config/initializers/constants.rb
@@ -0,0 +1,8 @@
+GITHUB_URL = 'https://github.com/Nerdman4U/unite-the-armies-2024'
+VESL_URL = 'https://www.vesl.fi'
+TEKNIIKKAELAMAAPALVELEMAAN_URL = "http://tekniikkaelamaapalvelemaan.fi"
+UNITE_ROOT_URL = 'https://unitethearmies.org'
+UNITE_FACEBOOK_URL = "https://www.facebook.com/unitethearmiesnow"
+UNITE_YOUTUBE_URL = "https://www.youtube.com/channel/UCbBytuGB3cDGyJ2RCSWpovw"
+UNITE_GOOGLEPLUS_URL = "https://plus.google.com/u/0/b/110514912421465474435/110514912421465474435/about/p/pub"
+UNITE_DOWNLOAD_URL = "http://unite-the-armies.org/en/download"
