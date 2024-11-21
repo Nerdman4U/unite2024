73c31e36ad4da61887d5036075e57b2747125da7
Joni Toyryla <info@jonitoyryla.eu>
2024-11-21 11:30:33 +0200

    reload votes.

--- app/javascript/start.js
+++ app/javascript/start.js
@@ -3,4 +3,7 @@ import UniteTheArmies from "unitethearmies";
 $(function () {
   let unite = new UniteTheArmies();
   unite.loadVotes();
+  setInterval(function () {
+    unite.loadVotes();
+  }, 5000);
 });
