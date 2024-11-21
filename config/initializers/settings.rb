0f709183f7880fa58278db2395f003ddbaca71be
Joni Toyryla <info@jonitoyryla.eu>
2024-11-21 14:28:46 +0200

    feat(config): disabled cache using database

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,8 +1,10 @@
 Rails.application.config.after_initialize do
+  next unless UaSetting.table_exists?
   uas = UaSetting.instance
   if uas.blank?
     throw "UaSetting not found"
   end
+
   uas.sent_count = UNITE_SENT_COUNT
   uas.save
 
