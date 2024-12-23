f5a810e20221cdb2023e452ff4f58ba5fdcd1699
Joni Toyryla <info@jonitoyryla.eu>
2024-12-23 15:08:39 +0200

    feat: Bootstrap icons, fixed indentation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,11 +26,9 @@ module ApplicationHelper
 
   ## Link to Token
   #
-  # <i> needs to be ended with </i>. Currenly, by default, tag-helper.i ends <i> as <i/> which is not enough
-  # because icons are multiplied (maybe because of bootstrap?). This why tag.i has empty content (by having do end).
   def link_to_token
     link_to new_token_url do
-      tag.i class: "bi bi-flower1", title: _("To your private vote") do end
+      tag.i class: "bi bi-flower1", title: _("To your private vote")
     end
   end
 
@@ -46,13 +44,64 @@ module ApplicationHelper
 
     token = vote.encoded_payload
     link_to vote_url(token: token) do
-      tag :i, class: "bi bi-chat-square", title: _("Your private vote")
+      tag.i class: "bi bi-chat-square", title: _("Your private vote")
     end
   end
 
   def link_to_logout
     link_to logout_url, method: :delete do
-      tag :i, class: "bi bi-box-arrow-right", title: _("Logout")
+      tag.i class: "bi bi-box-arrow-right", title: _("Logout")
     end
+  end
+
+  def button_white
+    skin_barber_shop_removed = "button tms-caption thick "
+    skin_barder_shop_stays = "bkg-white bkg-hover-cornflowerblue color-black color-hover-white text-uppercase "
+    # bootstrap = "py-4 px-6 fw-bold fs-3 position-static text-decoration-none shadow rounded border border-light opacity-100"
+    bootstrap = "py-4-5 px-6 fs-4 position-static opacity-100 shadow border border-2 border-light rounded"
+    skin_barber_shop_removed + skin_barder_shop_stays + bootstrap
+  end
+
+  def button_test
+    "button tms-caption thick bkg-black bkg-hover-cornflowerblue color-white color-hover-white text-uppercase py-4-5 px-6 fs-4 position-static opacity-100 shadow border border-2 border-light rounded"
+  end
+
+  def button_black
+    skin_barber_shop_removed = "button tms-caption thick "
+    skin_barder_shop_stays = "bkg-black bkg-hover-cornflowerblue color-white color-hover-white text-uppercase "
+    bootstrap = "py-4-5 px-6 fs-4 position-static opacity-100 shadow border border-2 border-light rounded"
+    skin_barber_shop_removed + skin_barder_shop_stays + bootstrap
+  end
+
+  def vimeo_stuff
+    if locale == :fi
+      url = "https://player.vimeo.com/video/153508732?autoplay=false"
+      video_id = "153508732"
+    elsif locale == :es
+      url = "https://player.vimeo.com/video/153508813?autoplay=false"
+      video_id = "153508813"
+    elsif locale == :zh
+      url = "https://player.vimeo.com/video/153508814?autoplay=false"
+      video_id = "153508814"
+    elsif locale == :ru
+      url = "https://player.vimeo.com/video/153508816?autoplay=false"
+      video_id = "153508816"
+    elsif locale == :fr
+      url = "https://player.vimeo.com/video/153508812?autoplay=false"
+      video_id = "153508812"
+    elsif locale == :ar
+      url = "https://player.vimeo.com/video/160513218?autoplay=false"
+      video_id = "160513218"
+    else
+      url = "https://player.vimeo.com/video/153508772?autoplay=false"
+      video_id = "153508772"
+    end
+    return url, video_id
+  end
+  def unite_video_id
+    vimeo_stuff[1]
+  end
+  def unite_video_url
+    vimeo_stuff[0]
   end
 end
