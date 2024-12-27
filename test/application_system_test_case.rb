3ad18ca953bc44eb9062f23238abd710a3a9ff13
Joni Toyryla <info@jonitoyryla.eu>
2024-12-27 11:24:31 +0200

    feat(application_system_test_case.rb): MobileSystemTestCase
    accidentally committed with previous bump, here is locale and default url at setup.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,11 +5,17 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
   driven_by :cuprite
 
+  def setup
+    I18n.locale = :en
+  end
+
 end
 
 class MobileSystemTestCase < ApplicationSystemTestCase
   def setup
     # Setup actions specific to mobile system tests can be added here
+    I18n.locale = :en
+    visit locale_root_path
     resize_window_to_mobile
   end
 
