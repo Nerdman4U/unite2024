7b8928076dc37ae035168cdb174cb6cb03ea7ffe
Joni Toyryla <info@jonitoyryla.eu>
2025-01-20 10:28:24 +0200

    feat: Header css fixes for mobile navigation

--- test/system/prism/browse_site_test.rb
+++ test/system/prism/browse_site_test.rb
@@ -14,6 +14,10 @@ class BrowseSiteTest < ApplicationSystemTestCase
     assert !page.secure?
     assert page.header.logo.present?
     assert page.footer
+
+    assert_selector 'h1', text: 'Save the Planet'.upcase
+    assert_no_selector '.header .mobile-navigation'
+    assert_selector '.header .primary-navigation'
   end
 
   test 'should redirect landing from page without locale' do
---
+++ test/system/prism/mobile_browse_site_test.rb
@@ -0,0 +1,26 @@
+require "application_system_test_case"
+
+class MobileBrowseSiteTest < MobileSystemTestCase
+
+  def setup
+    FastGettext.locale = :en
+    @app = ApplicationPrism.new
+  end
+
+  test 'should visit landing page' do
+    page = @app.welcome_index
+    page.load
+    assert_equal 'Home (Save the Planet - Unite the Armies)', page.title
+    assert !page.secure?
+    assert page.header.logo.present?
+    assert page.footer
+
+    assert_selector 'h1', text: 'Save the Planet'.upcase
+    assert_selector '.header .mobile-navigation'
+    assert_no_selector '.header .primary-navigation'
+  end
+
+
+
+
+end
