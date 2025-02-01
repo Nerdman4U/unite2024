faf1c73561cc115198fd5bf67c9bf79884aeeb4e
Joni Toyryla <info@jonitoyryla.eu>
2025-02-01 20:12:03 +0200

    fix: Copyright translation line fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -395,7 +395,7 @@ module ApplicationHelper
   end
 
   def copyright
-    _("&copy; 2015-%{year} SAVE THE PLANET - UNITE THE ARMIES. All Rights Reserved." % { year: Date.today.year }).html_safe
+    (_("&copy; 2015-%{year} SAVE THE PLANET - UNITE THE ARMIES. All Rights Reserved.") % { year: Date.today.year }).html_safe
   end
 
   def language_item identifier
